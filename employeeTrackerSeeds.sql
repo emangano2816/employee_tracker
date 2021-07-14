ce5c55c985f21bcb113fc125267c8e6ac2b93fda
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-14 11:16:50 -0400

    Modified code: included INSERT statements to populate test data in each table.

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -5,6 +5,7 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 -- Create department table --
+-- Parent table to emprole--
 CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
@@ -12,6 +13,8 @@ CREATE TABLE department (
 );
 
 -- Create role table --
+-- Child table of department--
+-- Parent table of employee --
 CREATE TABLE emprole (
     id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -23,6 +26,7 @@ CREATE TABLE emprole (
 );
 
 -- Create employee table --
+-- Child table of emprole
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -31,7 +35,27 @@ CREATE TABLE employee (
     manager_id INTEGER NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_role_id FOREIGN KEY (role_id) 
-    REFERENCES emprole(id),
+    REFERENCES emprole(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     CONSTRAINT FK_manager_id FOREIGN KEY (manager_id)
     REFERENCES emprole(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
+
+-- Populating tables with test data --
+INSERT INTO department (name) 
+VALUES('Accountability'), ('Testing'), ('Research');
+
+INSERT INTO emprole (title, salary, department_id) 
+VALUES ('Manager', '100000', 1), ('Data Specialist', '80000', 1);
+
+INSERT INTO emprole (title, salary, department_id) 
+VALUES ('Manager', '100000', 2), ('Testing Specialist', '85000',2);
+
+INSERT INTO emprole (title, salary, department_id)
+VALUES ('Research Specialist','90000',3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Beth', 'Thompson', 1, null), ('Clark', 'Account', 2, 1);
